eee31df47476c70fece33e2085fec6ee2f719f3e
djakovm <martindjakov03@gmail.com>
2024-11-09 22:44:08 +0100

    Add Cards Component to Home Page

--- imaps-frontend/src/pages/IMaps/components/pages/Home.jsx
+++ imaps-frontend/src/pages/IMaps/components/pages/Home.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
+import React from "react";
 import "./Home.scss";
-import {Link} from "react-router-dom";
-import {Button} from "../Button.jsx";
-
+import { Link } from "react-router-dom";
+import { Button } from "../Button.jsx";
+import Cards from "../Cards.jsx";
+import Footer from "../Footer.jsx";
 
 function Home() {
   return (
-        <div className="home">
-            <div className="hero-container">
-                <h1>Map Your World</h1>
-                <h2>Create and explore detailed indoor maps.</h2>
-                <div className="hero-btns">
-                    <Link to="/myMaps">
-                        <Button className="btns" buttonSize="btn--large">Create Maps</Button>
-                    </Link>
-                    <Link to="/Maps">
-                        <Button
-                            className="btns"
-                            buttonSize="btn--large"
-                        >
-                            Browse Maps <i className="far fa-play-circle"/>
-                        </Button>
-                    </Link>
-                </div>
-            </div>
-            {/* <Cards /> */}
+    <div className="home">
+      <div className="hero-container">
+        <h1>Map Your World</h1>
+        <h2>Create and explore detailed indoor maps.</h2>
+        <div className="hero-btns">
+          <Link to="/myMaps">
+            <Button className="btns" buttonSize="btn--large">
+              Create Maps
+            </Button>
+          </Link>
+          <Link to="/Maps">
+            <Button className="btns" buttonSize="btn--large">
+              Browse Maps <i className="far fa-play-circle" />
+            </Button>
+          </Link>
         </div>
+      </div>
+      <Cards />
+      <Footer></Footer>
+    </div>
   );
 }
 
