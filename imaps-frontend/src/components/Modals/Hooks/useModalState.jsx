e5dda0224ddc19d563a7c76e3c27c4efff2a35ba
stefan toskovski <stefantoska84@gmail.com>
2024-11-24 20:49:29 +0100

    Fix room types, highlight rooms by type not working on view

--- imaps-frontend/src/components/Modals/Hooks/useModalState.jsx
+++ imaps-frontend/src/components/Modals/Hooks/useModalState.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 
-export default function useModalState(formData,setFormData,map,getInitialFormData,eventName = null){
+export default function useModalState(formData,setFormData,map,getInitialFormData,eventName = null,setRoomTypes = null){
     const [isOpen, setIsOpen] = useState(false);
     const [shape, setShape] = useState(null);
     const [connections, setConnections] = useState([]);
