2382d42060f941ba377b83a1a1ec1a4b0dbef1ab
djakovm <martindjakov03@gmail.com>
2024-12-10 19:39:26 +0100

    New Navbar

--- imaps-frontend/src/pages/IMaps/components/Navbar.jsx
+++ imaps-frontend/src/pages/IMaps/components/Navbar.jsx
@@ -1,73 +1,40 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Button } from "./Button";
-import "./Navbar.css";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import logo_icon from "../../../assets/logo_icon.png";
 import { AuthContext } from "../../../components/AuthContext/AuthContext";
 import Logo from "../../../components/Logo/Logo.jsx";
 import Profile from "../../../components/Profile/Profile.jsx";
+import "./Navbar.css";
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+    const { isAuthenticated } = useContext(AuthContext);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+    return (
+        <nav className="modern-navbar">
+            <div className="navbar-container">
+                {/* Left Section - Logo and Title */}
+                <div className="navbar-left">
+                    <Logo position="relative"/>
+                    <h1 className="navbar-title">iMaps</h1>
+                </div>
 
-  const { isAuthenticated } = useContext(AuthContext);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", showButton);
-    return () => window.removeEventListener("resize", showButton);
-  }, []);
-
-  return (
-      <nav className="navbar">
-        <div className="navbar-container">
-          <div className="navlogoCont">
-            <Logo position="relative" />
-            <h1>iMAPS</h1>
-          </div>
-
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
-          </div>
-
-          <div className={click ? "nav-menu active" : "nav-menu"}></div>
-
-          <div className="linkCont">
-            {isAuthenticated ? (
-                <Profile position='inline'/>
-            ) : (
-                <>
-                  <div className="signup">
-                    <Link to="/Signup">
-                      {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
-                    </Link>
-                  </div>
-                  <div className="login">
-                    <Link to="/Login">
-                      {button && <Button buttonStyle="btn--outline">LOG IN</Button>}
-                    </Link>
-                  </div>
-                </>
-            )}
-          </div>
-        </div>
-      </nav>
-  );
+                {/* Right Section - Login/Signup or Profile */}
+                <div className="navbar-right">
+                    {isAuthenticated ? (
+                        <Profile position="relative"/>
+                    ) : (
+                        <>
+                            <Link to="/Login" className="navbar-btn navbar-login">
+                                Log In
+                            </Link>
+                            <Link to="/Signup" className="navbar-btn navbar-signup">
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
