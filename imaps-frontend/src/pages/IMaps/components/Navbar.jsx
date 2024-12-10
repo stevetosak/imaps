49965e4d1618819592101603a2d71a898755c42c
djakovm <martindjakov03@gmail.com>
2024-12-10 22:09:52 +0100

    Merge bug fix

--- imaps-frontend/src/pages/IMaps/components/Navbar.jsx
+++ imaps-frontend/src/pages/IMaps/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, {useContext, useState} from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../components/AuthContext/AuthContext";
 import Logo from "../../../components/Logo/Logo.jsx";
