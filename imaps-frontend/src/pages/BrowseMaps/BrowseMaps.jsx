e25e389761e5be21298005755a804fe71edce23d
djakovm <martindjakov03@gmail.com>
2024-12-22 14:17:03 +0100

    Add filter map to Browse maps

--- imaps-frontend/src/pages/BrowseMaps/BrowseMaps.jsx
+++ imaps-frontend/src/pages/BrowseMaps/BrowseMaps.jsx
@@ -1,28 +1,29 @@
 import styles from "./Maps.module.css";
 import "react-tiles-dnd/esm/index.css";
-import { TilesContainer } from "react-tiles-dnd";
-import { Link } from "react-router-dom";
+import {TilesContainer} from "react-tiles-dnd";
+import {Link} from "react-router-dom";
 import card from "../../assets/card-map.png";
 import star_icon from "../../assets/star_icon.png"; // Unfilled star icon
 import star_filled_icon from "../../assets/star_filled_icon.png"; // Filled star icon
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import HttpService from "../../scripts/net/HttpService.js";
 import Logo from "../../components/Logo/Logo.jsx";
 import Profile from "../../components/Profile/Profile.jsx";
 import config from "../../scripts/net/netconfig.js";
-import { useAppContext } from "../../components/AppContext/AppContext.jsx";
+import {useAppContext} from "../../components/AppContext/AppContext.jsx";
+import FilterMaps from "../../components/FilterMaps/FilterMaps.jsx";
 
 let loadedTiles = [];
 
-const renderTile = ({ data, isDragging, toggleFavorite }) => (
-    <div style={{ padding: "1rem", width: "100%", position: "relative" }}>
+const renderTile = ({data, isDragging, toggleFavorite}) => (
+    <div style={{padding: "1rem", width: "100%", position: "relative"}}>
         <Link to={`/Maps/View/${data.text}`} className={styles.linkStyle}>
             <div
                 className={`${styles.tile} ${isDragging ? styles.dragging : ""}`}
-                style={{ width: "100%", height: "100%" }}
+                style={{width: "100%", height: "100%"}}
             >
-                <img src={card} className={styles.imgStyle} alt="Map Thumbnail" />
-                <div style={{ fontFamily: "exo" }}>
+                <img src={card} className={styles.imgStyle} alt="Map Thumbnail"/>
+                <div style={{fontFamily: "exo"}}>
                     {data.text} {isDragging ? "DRAGGING" : null}
                 </div>
             </div>
@@ -31,7 +32,7 @@ const renderTile = ({ data, isDragging, toggleFavorite }) => (
             <img
                 src={data.isFavorite ? star_filled_icon : star_icon}
                 alt="Favorite Icon"
-                style={{ width: "20px", height: "20px" }}
+                style={{width: "20px", height: "20px"}}
             />
         </div>
     </div>
@@ -45,13 +46,15 @@ const tileSize = (tile) => ({
 export default function BrowseMaps() {
     const [searchTerm, setSearchTerm] = useState("");
     const [tiles, setTiles] = useState([]);
-    const { username, isAuthenticated } = useAppContext();
+    const {username, isAuthenticated} = useAppContext();
+    const [filter, setFilter] = useState("all")
+    const [mapFilters, setMapFilters] = useState([])
 
     useEffect(() => {
         const loadMaps = async () => {
             const httpService = new HttpService();
             let mapTiles = [];
-
+            let mapTypes = ['Hospital', 'Faculty', 'House', 'Other'];
             if (isAuthenticated) {
                 // :D
                 const favResp = await httpService.get(`${config.favourites.display}?username=${username}`);
@@ -62,11 +65,16 @@ export default function BrowseMaps() {
                     cols: 1,
                     rows: 1,
                     isFavorite: true,
+                    type: elem.mapType,
                 }));
 
+                console.log("TUKA")
                 // Load all maps
                 const allResp = await httpService.get(config.view_maps.display);
                 console.log("RESPONSE MAPS PUBLIC", allResp);
+
+                // mapTypes = allResp.filter(elem => elem.mapType != null && elem.mapType !== "").map(elem => elem.mapType);
+
 
                 const nonFavMapTiles = allResp
                     .filter((elem) => !favMapTiles.some((fav) => fav.text === elem.mapName))
@@ -75,20 +83,27 @@ export default function BrowseMaps() {
                         cols: 1,
                         rows: 1,
                         isFavorite: false,
+                        type: elem.mapType,
                     }));
+
 
                 mapTiles = [...favMapTiles, ...nonFavMapTiles];
             } else {
                 const allResp = await httpService.get(config.view_maps.display);
                 console.log("RESPONSE MAPS PUBLIC", allResp);
+                // mapTypes = allResp.filter(elem => elem.mapType != null && elem.mapType !== "").map(elem => elem.mapType);
 
                 mapTiles = allResp.map((elem) => ({
                     text: elem.mapName,
                     cols: 1,
                     rows: 1,
                     isFavorite: false,
+                    type: elem.mapType,
                 }));
             }
+
+            console.log("TYPES:", mapTypes);
+            setMapFilters(mapTypes);
 
             loadedTiles = [...mapTiles];
             sortTiles(mapTiles);
@@ -113,7 +128,7 @@ export default function BrowseMaps() {
         console.log("Response received:", response);
 
         const updatedTiles = tiles.map((tile) =>
-            tile.text === tileName ? { ...tile, isFavorite: !tile.isFavorite } : tile
+            tile.text === tileName ? {...tile, isFavorite: !tile.isFavorite} : tile
         );
 
         loadedTiles = [...updatedTiles];
@@ -139,6 +154,20 @@ export default function BrowseMaps() {
         });
     };
 
+    const onFilter = (selectedFilter) => {
+        setFilter(selectedFilter);
+
+        if (selectedFilter === "all") {
+            // Show all tiles
+            setTiles(loadedTiles);
+        } else {
+            // Filter tiles by selected type
+            const filteredTiles = loadedTiles.filter((tile) => tile.type === selectedFilter);
+            setTiles(filteredTiles);
+        }
+    };
+
+
     return (
         <div className={styles.container}>
             <h1>Explore Maps</h1>
@@ -151,11 +180,15 @@ export default function BrowseMaps() {
                     value={searchTerm}
                     onChange={handleSearchChange}
                 />
+
+            </div>
+            <div className={styles.filterBar}>
+            <FilterMaps mapTypes={mapFilters} setFilter={onFilter}></FilterMaps>
             </div>
 
             <TilesContainer
                 data={tiles}
-                renderTile={(props) => renderTile({ ...props, toggleFavorite })}
+                renderTile={(props) => renderTile({...props, toggleFavorite})}
                 tileSize={tileSize}
                 forceTileWidth={170}
                 forceTileHeight={200}
